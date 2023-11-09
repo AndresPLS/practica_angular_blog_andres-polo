17089adf4b189e2f4b281c818f2131d053b2fda8
Andres Polo <andrespolo13@gmail.com>
2023-11-09 18:26:30 +0100

    Inicialización de plantilla - Creación de componentes, páginas, servicio e interfaz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './pages/posts/posts.component';
+import { NewComponent } from './pages/new/new.component';
+import { ListaPostsComponent } from './components/lista-posts/lista-posts.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    NewComponent,
+    ListaPostsComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/interfaces/post-interface.ts
@@ -0,0 +1,9 @@
+export interface IPost {
+    id: number;
+    titulo: string;
+    autor: string;
+    imagen: string;
+    texto: string;
+    fecha: Date;
+    categoria: string;
+}
