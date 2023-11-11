3cd3fdaa2a17caddb464d1bac4c1e90a5acf8578
Andres Polo <andrespolo13@gmail.com>
2023-11-11 18:23:30 +0100

    Rutas - Creación componente Lista Post y estilos CSS

--- src/app/components/lista-posts/lista-posts.component.ts
+++ src/app/components/lista-posts/lista-posts.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
+import { IPost } from 'src/app/interfaces/post-interface';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
-  selector: 'app-lista-posts',
+  selector: 'lista-posts',
   templateUrl: './lista-posts.component.html',
   styleUrls: ['./lista-posts.component.css']
 })
 export class ListaPostsComponent {
+
+  postsService = inject(PostsService);
+
+  @Input() onePost: IPost;
+
+
+  constructor() {
+    this.onePost = {
+      id: 0,
+      titulo: '',
+      autor: '',
+      imagen: '',
+      texto: '',
+      fecha: '',
+      categoria: '',
+      ubicacion: '',
+    }
+
+  }
 
 }
