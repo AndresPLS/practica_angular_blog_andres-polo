c7fa57d4ae6e16eb4b32af905b258892b7736744
Andres Polo <andrespolo13@gmail.com>
2023-11-20 23:20:21 +0100

    Ajustes del array, nuevos datos.

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -17,25 +17,15 @@ export class PostsService {
         titulo: 'Emptiness',
         subtitulo: 'Un viaje sin color',
         autor: 'Nollan',
-        imagen: 'https://i.postimg.cc/KjYCHy7c/C0119-MP4-04-39-55-10-Imagen-fija001.jpg',
+        imagen: 'http://actouno.com/contenido/contenidoSlide/actouno-Web-Tamandua-r-2_2.jpg',
         texto: 'Mientras que para la cultura occidental el sentimiento de vacío está ligado a emociones, tales como la ansiedad o la depresión; en algunas filosofías orientales como el budismo y el taoísmo, el vacío verdadero que no está ligado a las emociones (conocido como Śūnyatā en el budismo), aparece como un estado superior de realización el cual es un medio o camino que nos permite llegar a alcanzar la iluminación espiritual. En las religiones y filosofías orientales, al ser un estado de vacío verdadero (al no estar ligado a las emociones), no tiene el mismo sentido que en la cultura occidental; ya que de esta forma se trata de un estado superior del ser humano, y no de un estado emocional que provoca malestar a través de los sentimientos negativos.',
         fecha: '6/15/15',
-        categoria: 'Abstrac',
+        categoria: 'Island',
         ubicacion: 'Internet',
       },
       {
+
         id: 2,
-        titulo: 'Tamandua',
-        subtitulo: 'Un viaje sin color',
-        autor: 'Ignacio Nuevo & Andrés Polo',
-        imagen: 'http://actouno.com/contenido/contenidoSlide/actouno-Web_Tamandua_G2-5_2.jpg',
-        texto: 'Mientras que para la cultura occidental el sentimiento de vacío está ligado a emociones, tales como la ansiedad o la depresión; en algunas filosofías orientales como el budismo y el taoísmo, el vacío verdadero que no está ligado a las emociones (conocido como Śūnyatā en el budismo), aparece como un estado superior de realización el cual es un medio o camino que nos permite llegar a alcanzar la iluminación espiritual. En las religiones y filosofías orientales, al ser un estado de vacío verdadero (al no estar ligado a las emociones), no tiene el mismo sentido que en la cultura occidental; ya que de esta forma se trata de un estado superior del ser humano, y no de un estado emocional que provoca malestar a través de los sentimientos negativos.',
-        fecha: '6/15/15',
-        categoria: 'Nature',
-        ubicacion: 'Panamá, PNM',
-      },
-      {
-        id: 3,
         titulo: 'Panama city',
         subtitulo: 'Un viaje sin color',
         autor: 'Nollan',
@@ -44,6 +34,17 @@ export class PostsService {
         fecha: '6/15/15',
         categoria: 'City',
         ubicacion: 'Panamá, PNM',
+      },
+      {
+        id: 3,
+        titulo: 'Bachimaña',
+        subtitulo: 'Un viaje sin color',
+        autor: 'Nollan',
+        imagen: 'https://refugiodelosibones.files.wordpress.com/2022/05/bachimana-atardecer-1.jpg',
+        texto: 'Mientras que para la cultura occidental el sentimiento de vacío está ligado a emociones, tales como la ansiedad o la depresión; en algunas filosofías orientales como el budismo y el taoísmo, el vacío verdadero que no está ligado a las emociones (conocido como Śūnyatā en el budismo), aparece como un estado superior de realización el cual es un medio o camino que nos permite llegar a alcanzar la iluminación espiritual. En las religiones y filosofías orientales, al ser un estado de vacío verdadero (al no estar ligado a las emociones), no tiene el mismo sentido que en la cultura occidental; ya que de esta forma se trata de un estado superior del ser humano, y no de un estado emocional que provoca malestar a través de los sentimientos negativos.',
+        fecha: '6/15/15',
+        categoria: 'Mountain',
+        ubicacion: 'Huesca, ESP',
       },
       {
         id: 4,
@@ -58,14 +59,14 @@ export class PostsService {
       },
       {
         id: 5,
-        titulo: 'Bachimaña',
+        titulo: 'Tamandua',
         subtitulo: 'Un viaje sin color',
-        autor: 'Nollan',
-        imagen: 'https://refugiodelosibones.files.wordpress.com/2022/05/bachimana-atardecer-1.jpg',
+        autor: 'Ignacio Nuevo & Andrés Polo',
+        imagen: 'http://actouno.com/contenido/contenidoSlide/actouno-Web_Tamandua_G2-5_2.jpg',
         texto: 'Mientras que para la cultura occidental el sentimiento de vacío está ligado a emociones, tales como la ansiedad o la depresión; en algunas filosofías orientales como el budismo y el taoísmo, el vacío verdadero que no está ligado a las emociones (conocido como Śūnyatā en el budismo), aparece como un estado superior de realización el cual es un medio o camino que nos permite llegar a alcanzar la iluminación espiritual. En las religiones y filosofías orientales, al ser un estado de vacío verdadero (al no estar ligado a las emociones), no tiene el mismo sentido que en la cultura occidental; ya que de esta forma se trata de un estado superior del ser humano, y no de un estado emocional que provoca malestar a través de los sentimientos negativos.',
         fecha: '6/15/15',
-        categoria: 'Mountain',
-        ubicacion: 'Huesca, ESP',
+        categoria: 'Nature',
+        ubicacion: 'Panamá, PNM',
       },
       {
         id: 6,
@@ -100,16 +101,27 @@ export class PostsService {
         categoria: 'Abstrac',
         ubicacion: 'Internet',
       },
+      {
+        id: 9,
+        titulo: 'Emptiness',
+        subtitulo: 'Un viaje sin color',
+        autor: 'Nollan',
+        imagen: 'https://i.postimg.cc/KjYCHy7c/C0119-MP4-04-39-55-10-Imagen-fija001.jpg',
+        texto: 'Mientras que para la cultura occidental el sentimiento de vacío está ligado a emociones, tales como la ansiedad o la depresión; en algunas filosofías orientales como el budismo y el taoísmo, el vacío verdadero que no está ligado a las emociones (conocido como Śūnyatā en el budismo), aparece como un estado superior de realización el cual es un medio o camino que nos permite llegar a alcanzar la iluminación espiritual. En las religiones y filosofías orientales, al ser un estado de vacío verdadero (al no estar ligado a las emociones), no tiene el mismo sentido que en la cultura occidental; ya que de esta forma se trata de un estado superior del ser humano, y no de un estado emocional que provoca malestar a través de los sentimientos negativos.',
+        fecha: '6/15/15',
+        categoria: 'Abstrac',
+        ubicacion: 'Internet',
+      },
 
     ]
 
     this.arrFiltradoCategorias = [];
 
-    /*     
-    if (localStorage.getItem('arr_posts')) {
-      this.arrPosts = JSON.parse(localStorage.getItem('arr_post')!)
+
+    if (localStorage.getItem('array_newPost')) {
+      this.arrPosts = JSON.parse(localStorage.getItem('array_newPost')!)
     }
-     */
+
   }
 
 
@@ -138,5 +150,9 @@ export class PostsService {
     return this.arrPosts.find(post => post.id === postId);
   }
 
+  metodoCreate(elementoPost: IPost) {
+    this.arrPosts.push(elementoPost);
+    localStorage.setItem('array_newPost', JSON.stringify(this.arrPosts));
+  }
 
 }
